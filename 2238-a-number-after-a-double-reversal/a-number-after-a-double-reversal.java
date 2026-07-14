f2100055124a759ac77c737a673b753fd9283020
harshavardhandereddy <dereddyharsha3@gmail.com>
2026-07-14 15:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.90%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int reverse(int num){
+        int a = 0;
+        while(num != 0){
+            int ld = num % 10;
+            a = a * 10 + ld;
+            num = num/10;
+        }
+        return a;
+    }
+    public boolean isSameAfterReversals(int num) {
+        return num == reverse(reverse(num));
+
+    }
+}
