e529ddb2bb255935635459ee74d4511fd8895124
harshavardhandereddy <dereddyharsha3@gmail.com>
2026-08-08 09:45:36 +0530

    Time: 1 ms (98.23%) | Memory: 43.5 MB (18.76%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int ans=0;
+        for(int i=0;i<s.length()-1;i++){
+            int a=i;
+            int b=i+1;
+            char first=s.charAt(a);
+            char second= s.charAt(b);
+            int ascii=first;
+            int bscii=second;
+            int temp=Math.abs(ascii-bscii);
+            ans=ans+temp;
+
+        }
+        return ans;
+        
+    }
+}
